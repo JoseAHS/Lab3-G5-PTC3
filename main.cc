8472467a7daac50bcbe3273d96fdc89b93810767
Jose <00031323@uca.edu.sv>
2024-09-03 10:20:01 -0600

    feat: Agregar SumaRecursiva

--- main.cc
+++ main.cc
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+int SumaRecursiva(int n) {
+    if (n == 0) {
+        return 0;
+    }
+        return n + SumaRecursiva(n - 1);
+}
+
+
+int main() {
+    int n;
+
+    std::cout << "Ingrese un número natural: ";
+    std::cin >> n;
+
+    if (n < 0) {
+        std::cout << "El número debe ser un natural (mayor o igual a 0)." << std::endl;
+        return 1;
+    }
+
+    std::cout << "La suma de los primeros " << n << " números naturales es: " << SumaRecursiva(n) << std::endl;
+
+    
+
     return 0;
 }
+
