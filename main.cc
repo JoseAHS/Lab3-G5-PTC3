c5a64d9b651ac3a2469415270b47998c947a5758
unknown <00095023@uca.edu.sv>
2024-09-03 10:16:23 -0600

    Feat: Crear codigo para calcular potencias
    Se agreago el codigo donde esta la funcion para calcular el exponente de un numero

--- main.cc
+++ main.cc
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+// Función para calcular la potencia de un número
+int potencia(int num, int exponente) {
+    if (exponente == 0)
+        return 1;
+    else
+        return num * potencia(num, exponente - 1);
+}
+
+int main() {
+    int num, exponente;
+
+    std::cout << "Introduce el numero: ";
+    std::cin >> num;
+    std::cout << "Introduce el exponente: ";
+    std::cin >> exponente;
+
+    int resultado = potencia(num, exponente);
+    std::cout << num<< "^" << exponente << " = " << resultado << std::endl;
+    
     return 0;
 }
